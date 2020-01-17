d195187c0207ba0d12c601e255a8bbabca7b798b
Edras Altamirano <altamiranoesdras@gmail.com>
2020-01-17 13:10:50 -0600

    Mejora menu sidebar para niveles infinitos (vista árbol/treeview)

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -11,30 +11,8 @@
         </a>
         @if($option->hasChildren())
             <ul class="nav nav-treeview">
-                @foreach($option->children as $index => $option)
-
-                    <li class="nav-item {{$option->hasTreeview()}} {{$option->openTreeView()}}">
-                        <a href="{{rutaOpcion($option)}}" class="nav-link {{$option->active()}}">
-                            <i class="nav-icon fa {{$option->icono_l}}"></i>
-                            <p>
-                                {{$option->nombre}}
-                                @if($option->hasChildren())
-                                    <i class="right fa fa-angle-left"></i>
-                                @elseif($option->icono_r)
-                                    <span class="right badge badge-danger">New</span>
-                                @endif
-                            </p>
-                        </a>
-                        @if($option->hasChildren())
-                            <ul class="nav nav-treeview">
-                                @include('layouts.partials.menu',['opciones' => $option->children])
-                            </ul>
-                        @endif
-                    </li>
-
-                @endforeach
+                @include('layouts.partials.menu',['opciones' => $option->children])
             </ul>
         @endif
-
     </li>
 @endforeach
