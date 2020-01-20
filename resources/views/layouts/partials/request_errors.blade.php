ff802afc2d7e5082f19684c7498d4179a5c49a95
Edras Altamirano <altamiranoesdras@gmail.com>
2020-01-20 16:38:36 -0600

    Creado partial request_errors.blade.php
    Editadas plantillas infyom
    
    Agregado SpanishDataTables.json

---
+++ resources/views/layouts/partials/request_errors.blade.php
@@ -0,0 +1,12 @@
+@if(!empty($errors))
+    @if ($errors->any())
+        <div class="alert alert-danger alert-error">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+@endif
