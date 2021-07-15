64b507225452b856c3f39a704073028d68f50ee9
Edras Altamirano <altamiranoesdras@gmail.com>
2021-07-15 09:36:02 -0600

    Migración de pruebas

--- database/migrations/2021_07_15_093135_create_tareas_table.php
+++ database/migrations/2021_07_15_093135_create_tareas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotasTable extends Migration
+class CreateTareasTable extends Migration
 {
     /**
      * Run the migrations.
