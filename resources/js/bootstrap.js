007d8659bd42aedd84726f725c2dee5a311062eb
Edras Altamirano <altamiranoesdras@gmail.com>
2022-05-14 11:15:05 -0600

    Token csrf para peticiones ajax

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,41 +1,63 @@
 window._ = require('lodash');
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+
 
 try {
     // window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
+    window.Swal = require('sweetalert2');
+
 
 } catch (e) {}
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
 
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
-// import Echo from 'laravel-echo';
 
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+// import Echo from "laravel-echo"
+//
 // window.Pusher = require('pusher-js');
-
+//
 // window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
+//     broadcaster:       'pusher',
+//     key:               process.env.MIX_PUSHER_APP_KEY,
+//     wsHost:            window.location.hostname,
+//     wsPort:            6002,
+//     wssPort:           6002,
+//     disableStats:      false,
+//     encrypted:         true,
+//     enabledTransports: ['ws', 'wss'],
 // });
+
+
+// require('webrtc-adapter');
+// window.Cookies = require('js-cookie');
+//
+// import Echo from "laravel-echo"
+//
+// window.io = require('socket.io-client');
+//
+// window.Echo = new Echo({
+//     broadcaster: 'socket.io',
+//     host: window.location.hostname + ':6001'
+// });
+
