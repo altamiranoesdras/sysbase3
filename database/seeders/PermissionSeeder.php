7364b0099ce0a0b177d54f8c4d9f16112b555db5
Edras Altamirano <altamiranoesdras@gmail.com>
2022-08-23 08:23:12 -0600

    Fix seeder permisos

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,33 +16,30 @@ class PermissionSeeder extends Seeder
     public function run()
     {
 
-        foreach (Route::getRoutes() as $route){
+        Permission::firstOrCreate(['name' => 'Ver configuración']);
+        Permission::firstOrCreate(['name' => 'Crear configuración']);
+        Permission::firstOrCreate(['name' => 'Editar configuración']);
+        Permission::firstOrCreate(['name' => 'Eliminar configuración']);
 
-            Permission::firstOrCreate(['name' => 'Ver configuración']);
-            Permission::firstOrCreate(['name' => 'Crear configuración']);
-            Permission::firstOrCreate(['name' => 'Editar configuración']);
-            Permission::firstOrCreate(['name' => 'Eliminar configuración']);
+        Permission::firstOrCreate(['name' => 'Ver opcion menu']);
+        Permission::firstOrCreate(['name' => 'Crear opcion menu']);
+        Permission::firstOrCreate(['name' => 'Editar opcion menu']);
+        Permission::firstOrCreate(['name' => 'Eliminar opcion menu']);
 
-            Permission::firstOrCreate(['name' => 'Ver opcion menu']);
-            Permission::firstOrCreate(['name' => 'Crear opcion menu']);
-            Permission::firstOrCreate(['name' => 'Editar opcion menu']);
-            Permission::firstOrCreate(['name' => 'Eliminar opcion menu']);
+        Permission::firstOrCreate(['name' => 'Ver permisos']);
+        Permission::firstOrCreate(['name' => 'Crear permisos']);
+        Permission::firstOrCreate(['name' => 'Editar permisos']);
+        Permission::firstOrCreate(['name' => 'Eliminar permisos']);
 
-            Permission::firstOrCreate(['name' => 'Ver permisos']);
-            Permission::firstOrCreate(['name' => 'Crear permisos']);
-            Permission::firstOrCreate(['name' => 'Editar permisos']);
-            Permission::firstOrCreate(['name' => 'Eliminar permisos']);
+        Permission::firstOrCreate(['name' => 'Ver roles']);
+        Permission::firstOrCreate(['name' => 'Crear roles']);
+        Permission::firstOrCreate(['name' => 'Editar roles']);
+        Permission::firstOrCreate(['name' => 'Eliminar roles']);
 
-            Permission::firstOrCreate(['name' => 'Ver roles']);
-            Permission::firstOrCreate(['name' => 'Crear roles']);
-            Permission::firstOrCreate(['name' => 'Editar roles']);
-            Permission::firstOrCreate(['name' => 'Eliminar roles']);
-
-            Permission::firstOrCreate(['name' => 'Ver usuarios']);
-            Permission::firstOrCreate(['name' => 'Crear usuarios']);
-            Permission::firstOrCreate(['name' => 'Editar usuarios']);
-            Permission::firstOrCreate(['name' => 'Eliminar usuarios']);
-        }
+        Permission::firstOrCreate(['name' => 'Ver usuarios']);
+        Permission::firstOrCreate(['name' => 'Crear usuarios']);
+        Permission::firstOrCreate(['name' => 'Editar usuarios']);
+        Permission::firstOrCreate(['name' => 'Eliminar usuarios']);
 
     }
 }
